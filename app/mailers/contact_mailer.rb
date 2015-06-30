8f1c132c6750932d92ac1af8e4510b26bbcc53b0
Edgar Martinez <edgar.martinez.bazarte@gmail.com>
2015-06-30 05:35:15 +0000

    Added mailer and view

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,6 @@
-class ContactMailer < Action Mailer::Base
+class ContactMailer < ActionMailer::Base
    default to: 'edgar.martinez.bazarte@gmail.com'
+   
    def contact_email(name, email, body)
       @name = name
       @email = email
